7d8d84b21ba35d23f6d05063c20bb6eebe01c131
Fatima Falath <fatimafalath@FF-2.local>
2020-07-21 04:52:41 +0300

    Initial

---
+++ .ipynb_checkpoints/keys-checkpoint.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "consumer_key = 'eaG0EB9hQKgda5hELHKeuRCk2'\n",
+    "consumer_secret = 'QV4J8sNzn2YUcBTAKs1QHvdnYmZak49o0pLft0wdwJwgLRqRtf'\n",
+    "access_token = '148249796-90QYS0vfZ5aCeSdKZQ8I7JaqwpMdQ61uPQ8Ipj3L'\n",
+    "access_secret = 'kyzO58MRYW9zKYqptUjxMhKT8SOohIQDUUBYRRKeJ8FI1'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
